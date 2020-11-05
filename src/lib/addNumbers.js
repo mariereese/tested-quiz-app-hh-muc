85be0e470a478911254134e398c5015018445b25
Jeremias Erbs <jerry@badabam.de>
2020-11-05 11:53:30 +0100

    Fix addNumbers tests (with reduce/forEach)

--- src/lib/addNumbers.js
+++ src/lib/addNumbers.js
@@ -1,3 +1,9 @@
-export default function addNumbers(first, second) {
-  return first + second
+export default function addNumbers(...numbers) {
+  return numbers.reduce((result, number) => {
+    return result + number
+  }, 0)
+
+  // let result = 0
+  // numbers.forEach((number) => (result += number))
+  // return result
 }
