71e05cb5c1bd58c4146ac21b321daefdfe9be25a
Jeremias Erbs <jerry@badabam.de>
2020-11-05 11:53:56 +0100

    Remove commented forEach from addNumbers

--- src/lib/addNumbers.js
+++ src/lib/addNumbers.js
@@ -2,8 +2,4 @@ export default function addNumbers(...numbers) {
   return numbers.reduce((result, number) => {
     return result + number
   }, 0)
-
-  // let result = 0
-  // numbers.forEach((number) => (result += number))
-  // return result
 }
